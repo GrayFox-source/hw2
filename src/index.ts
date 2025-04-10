8662656c226970cad35b5cab74d1eddc1e75a57a
Frex <smolnikov2023@list.ru>
2025-04-10 20:52:52 +0300

    First API - BLOGS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import bodyParser from 'body-parser'
 import {blogsRouter} from "./routes/blogs-router";
 import {BlogViewModel} from "./models/BlogViewModel";
